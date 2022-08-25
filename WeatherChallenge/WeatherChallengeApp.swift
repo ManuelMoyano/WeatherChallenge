7f525ae293e50f748531761af8758fe8a4fca3ca
Manuel Moyano <manuelm_98@hotmail.com>
2022-08-25 11:49:24 -0300

    Initial Commit

---
+++ WeatherChallenge/WeatherChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherChallengeApp.swift
+//  WeatherChallenge
+//
+//  Created by Manuel Moyano on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
